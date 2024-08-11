1134b08af6510b45658a277dc44e8eca3824ce06
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2024-08-11 11:15:23 -0400

    pre refactor boiler added

--- Overworld.js
+++ Overworld.js
@@ -12,11 +12,25 @@ class Overworld {
         map.onload = () => {
             this.ctx.drawImage(map, 0, 0)
         };
-        map.src = "/images/placeholdermap.png";
+        map.src = "/images/map_placeholder.png";
+
+        
+
+        //offset for feet positioning
+        const offsetX = 9;
+        const offsetY = 5;
+        //allows coordinate based positioning
+        const gridSize = 16;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(shadow, (1 * gridSize) - offsetX, (4 * gridSize) - offsetY)
+        };
+        shadow.src = "/images/shadow_placeholder.png";
 
         const hero = new Image();
         hero.onload = () => {
-            this.ctx.drawImage(hero, 65, 50)
+            this.ctx.drawImage(hero, (1 * gridSize) - offsetX, (4 * gridSize) - offsetY)
         };
         hero.src = "/images/hero_placeholder.png";
     }
